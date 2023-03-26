0b4056811caa609c4d02b6987b4eb1a642e2f8fd
Adam Revezzo <adam.revezzo@gmail.com>
2023-03-26 14:57:32 -0400

    build logic for player results

--- ddl.sql
+++ ddl.sql
@@ -10,30 +10,69 @@ CREATE TABLE players IF NOT EXISTS (
 );
 
 CREATE TABLE IF NOT EXISTS player_results  (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     player_id INT NOT NULL REFERENCES players(id) ON DELETE CASCADE,
 	tournament_id INT NOT NULL REFERENCES tournaments(id) ON DELETE CASCADE,
-    tournament_end_date VARCHAR(80) NOT NULL
+    tournament_end_date VARCHAR(80) NOT NULL,
     finish_position VARCHAR(10) NOT NULL,
     r1 INT,
 	r2 INT,
 	r3 INT,
 	r4 INT,
     total INT,
+    to_par VARCHAR(10),
     points_rank INT,
 	tournament_points FLOAT
+);
 );
 
 CREATE TABLE IF NOT EXISTS tournaments  (
     id SERIAL PRIMARY KEY,
     pga_tournament_id VARCHAR(50) NOT NULL,
-	tournament_name VARCHAR(255) NOT NULL,
+	tournament_num INT NOT NULL,
+    tournament_name VARCHAR(255) NOT NULL,
     first_place_earnings VARCHAR(80) NOT NULL,
     purse VARCHAR(80) NOT NULL,
 	city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
 	country VARCHAR(255) NOT NULL,
 	course_name VARCHAR(255) NOT NULL,
     month VARCHAR(50) NOT NULL,
     year VARCHAR(6) NOT NULL,
     season_year VARCHAR(50) NOT NULL
 );
+
+-- Create Courses Table from Tournament Data and remove duplicate columns from Tournaments
+CREATE TABLE courses (
+	ID SERIAL PRIMARY KEY,
+	course_name VARCHAR(255) NOT NULL,
+	course_id_pga VARCHAR(10),
+	city VARCHAR(255),
+	state VARCHAR(255),
+	country VARCHAR(255)
+
+);
+
+INSERT INTO courses (course_name, city, state, country)
+SELECT 
+DISTINCT 
+course_name,
+city,
+state,
+country
+
+FROM tournaments;
+
+ALTER TABLE tournaments
+ADD column course_id INT;
+
+UPDATE tournaments
+SET course_id = courses.id
+FROM courses 
+WHERE tournaments.course_name = courses.course_name; 
+
+ALTER TABLE tournaments
+DROP COLUMN city,
+DROP COLUMN state,
+DROP COLUMN country,
+DROP COLUMN course_name;
--- schedule_scrape.py
+++ schedule_scrape.py
@@ -3,21 +3,26 @@ import time
 import json
 from database import Database
 
-YEARS = ["2013","2014","2015", "2016", "2017", "2018", "2019"]
+# YEARS = ["2013","2014","2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
+YEARS = ['2023']
 
 url = "https://orchestrator.pgatour.com/graphql"
 
-with open("schedule_header.json") as data:
-    headers = json.load(data)
+with open("schedule_header.json") as data_completed:
+    headers = json.load(data_completed)
 
 for year in YEARS:
     print(f"Running {year} Query")
     payload = f"{{\"query\":\"query Schedule($tourCode: String!, $year: String) {{\\n  schedule(tourCode: $tourCode, year: $year) {{\\n    completed {{\\n      tournaments {{\\n        tournamentName\\n        id\\n        beautyImage\\n        champion\\n        champions {{\\n          displayName\\n          playerId\\n        }}\\n        championEarnings\\n        championId\\n        city\\n        country\\n        countryCode\\n        courseName\\n        date\\n        dateAccessibilityText\\n        purse\\n        sortDate\\n        startDate\\n        state\\n        stateCode\\n        status {{\\n          roundDisplay\\n          roundStatus\\n          roundStatusColor\\n          roundStatusDisplay\\n        }}\\n        ticketsURL\\n        tourStandingHeading\\n        tourStandingValue\\n        tournamentLogo\\n        display\\n        sequenceNumber\\n      }}\\n      month\\n      monthSort\\n      year\\n    }}\\n    seasonYear\\n    tour\\n    seasonYear\\n    upcoming {{\\n      month\\n      year\\n      tournaments {{\\n        id\\n        date\\n        startDate\\n        dateAccessibilityText\\n        tournamentName\\n        tournamentLogo\\n        city\\n        state\\n        stateCode\\n        country\\n        countryCode\\n        courseName\\n        champion\\n        championId\\n        champions {{\\n          displayName\\n          playerId\\n        }}\\n        championEarnings\\n        beautyImage\\n        status {{\\n          roundStatusDisplay\\n          roundDisplay\\n          roundStatus\\n          roundStatusColor\\n        }}\\n        sortDate\\n        sequenceNumber\\n        purse\\n        ticketsURL\\n        tourStandingHeading\\n        tourStandingValue\\n        tournamentLogo\\n        tournamentName\\n        display\\n        sequenceNumber\\n      }}\\n      monthSort\\n    }}\\n    completed {{\\n      month\\n      year\\n      monthSort\\n      tournaments {{\\n        id\\n        date\\n        startDate\\n        dateAccessibilityText\\n        tournamentName\\n        tournamentLogo\\n        city\\n        state\\n        stateCode\\n        country\\n        countryCode\\n        courseName\\n        champion\\n        championId\\n        champions {{\\n          displayName\\n          playerId\\n        }}\\n        championEarnings\\n        beautyImage\\n        status {{\\n          roundStatusDisplay\\n          roundDisplay\\n          roundStatus\\n          roundStatusColor\\n        }}\\n        sortDate\\n        sequenceNumber\\n        purse\\n        ticketsURL\\n        tourStandingHeading\\n        tourStandingValue\\n        display\\n      }}\\n    }}\\n  }}\\n}}\",\"operationName\":\"Schedule\",\"variables\":{{\"tourCode\":\"R\",\"year\":\"{year}\"}}}}"
 
-    response = requests.request("POST", url, data=payload, headers=headers)
+    # response = requests.request("POST", url, data=payload, headers=headers)
 
-    all_data = response.json()
-    data = all_data['data']['schedule']['completed']
+    with open("2022_schedule.json") as data:
+        all_data = json.load(data)
+
+    # all_data = response.json()
+    data_completed = all_data['data']['schedule']['completed']
+    data_upcoming = all_data['data']['schedule']['upcoming']
     season = all_data['data']['schedule']['seasonYear'] 
 
     db = Database()
@@ -28,9 +33,11 @@ for year in YEARS:
             INSERT INTO tournaments (
                 pga_tournament_id,
                 tournament_name,
+                tournament_num,
                 first_place_earnings,
                 purse,
                 city,
+                state,
                 country,
                 course_name,
                 month,
@@ -40,35 +47,40 @@ for year in YEARS:
                 
             
             VALUES
-            (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             """
 
-            for tournament_month in data:
-                month = tournament_month['month']
-                year = tournament_month['year']
-                for tournament in tournament_month['tournaments']:
-                    pga_tournament_id = tournament['id']
-                    tournament_name = tournament['tournamentName']
-                    first_place_earnings = tournament['championEarnings']
-                    purse = tournament['purse']
-                    city = tournament['city']
-                    country = tournament['country']
-                    course = tournament['courseName']
+            for status in (data_completed, data_upcoming):
+                for tournament_month in status:
+                    month = tournament_month['month']
+                    year = tournament_month['year']
+                    for tournament in tournament_month['tournaments']:
+                        pga_tournament_id = tournament['id']
+                        tournament_name = tournament['tournamentName']
+                        tournament_num = (pga_tournament_id[-3:])
+                        first_place_earnings = tournament['championEarnings']
+                        purse = tournament['purse']
+                        city = tournament['city']
+                        state = tournament['state']
+                        country = tournament['country']
+                        course = tournament['courseName']
 
-                    data = (
-                        pga_tournament_id, 
-                        tournament_name, 
-                        first_place_earnings, 
-                        purse, 
-                        city,
-                        country, 
-                        course,
-                        month,
-                        year,
-                        season
-                        )
-                    cur.execute(insert_query, data)
-                    conn.commit()
+                        data_completed = (
+                            pga_tournament_id, 
+                            tournament_name,
+                            tournament_num, 
+                            first_place_earnings, 
+                            purse, 
+                            city,
+                            state,
+                            country, 
+                            course,
+                            month,
+                            year,
+                            season
+                            )
+                        cur.execute(insert_query, data_completed)
+                        conn.commit()
 
     db.close()
 
