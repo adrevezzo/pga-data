97b5de9a56d29587c2e5904827f3fc56fcfc7313
Adam Revezzo <adam.revezzo@gmail.com>
2023-03-28 09:42:26 -0400

    general code cleanup

--- player_results_by_year_scrape.py
+++ player_results_by_year_scrape.py
@@ -1,8 +1,6 @@
 from database import Database
 import time
 import requests
-from bs4 import BeautifulSoup
-import re
 import json
 
 # Static data for testing
@@ -29,10 +27,11 @@ all_players = [(result.get("pga_id"),result.get("player_name")) for result in re
 
 url = "https://orchestrator.pgatour.com/graphql"
 querystring = {"":""}
+
 with open("player_results_header.json") as data_completed:
     headers = json.load(data_completed)
 
-for player in all_players[30:100]:
+for player in all_players[200:]:
     payload = f"{{\"query\":\"\\tquery PlayerProfileSeasonResults($playerId: ID!, $tourCode: TourCode, $year: Int) {{\\n  playerProfileSeasonResults(\\n    playerId: $playerId\\n    tourCode: $tourCode\\n    year: $year\\n  ) {{\\n    playerId\\n    tour\\n    displayYear\\n    year\\n    events\\n    wins\\n    top10\\n    top25\\n    cutsMade\\n    missedCuts\\n    withdrew\\n    runnerUp\\n    seasonPills {{\\n      tourCode\\n      years {{\\n        year\\n        displaySeason\\n      }}\\n    }}\\n    cupRank\\n    cupPoints\\n    cupName\\n    cupLogo\\n    cupLogoDark\\n    cupLogoAccessibilityText\\n    rankLogo\\n    rankLogoDark\\n    rankLogoAccessibilityText\\n    officialMoney\\n    tournaments {{\\n      tournamentId\\n      tournamentEndDate\\n      tournamentName\\n      finishPosition\\n      r1\\n      r2\\n      r3\\n      r4\\n      r5\\n      total\\n      toPar\\n      pointsRank\\n      points\\n      tourcastURL\\n      tourcastURLWeb\\n    }}\\n    seasonRecap {{\\n      tourCode\\n      displayMostRecentSeason\\n      mostRecentRecapYear\\n      items {{\\n        year\\n        displaySeason\\n        items {{\\n          tournamentId\\n          year\\n          title\\n          body\\n        }}\\n      }}\\n    }}\\n    amateurHighlights\\n    tourcastEligible\\n  }}\\n}}\\n\\n\\n\\t\",\"operationName\":\"PlayerProfileSeasonResults\",\"variables\":{{\"playerId\":\"{player[0]}\",\"tourCode\":\"R\",\"year\":2023}}}}"
     response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
     all_data = response.json()
