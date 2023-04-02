b6b4a0071a3eb7c516ea020110ff62c4b2e165bb
Adam Revezzo <adam.revezzo@gmail.com>
2023-04-02 13:45:29 -0400

    create regex dictionary to reduce length of stat names

--- stats_scrape.py
+++ stats_scrape.py
@@ -6,48 +6,151 @@ import queries
 import re
 from utils import create_json_file
 
-# stat_id_dict = {}
-# # NON_ALPHA_PATTERN = re.compile(r'\W+')
-# NON_ALPHA_PATTERN = re.compile(r"[:\.,\s,\(,\)']+")
+stat_id_dict = {}
 
-# with open("stat_list.json") as stat_list:
-#     all_stats = json.load(stat_list)
-#     categories = all_stats['pageProps']['statOverview']['categories']
+EXTRA_WORDS = ('by', 'measured', '_', '®', 'from', 'the')
 
-# for category in categories:
-#     for sub in category['subCategories']:
-#         for stat in sub['stats']:
-#             if stat_id_dict.get(stat.get('statId')):
-#                 continue
-#             else:
-#                 stat_id_dict[stat.get('statId')] = stat.get('statTitle')
+PATTERN_DICT = {
+    'NON_ALPHA_PATTERN' : {
+        'pattern' : re.compile(r"[:\.,\s,\(,\)']+"),
+        'replace_string' : ""
+    },
 
-# for stat_id, stat_name in stat_id_dict.items():
-#     name_as_list = stat_name.split()
+    'APPROACH_PATTERN' : {
+        'pattern' : re.compile(r"^[a-zA-Z]pproach[a-zA-Z]*"),
+        'replace_string' : 'App'
+    },
 
-#     for i,chunk in enumerate(name_as_list):
-          
-#         chunk = chunk.replace("-","_")
-#         name_as_list[i] = chunk
-               
-#         file_match = NON_ALPHA_PATTERN.search(chunk)
-#         if file_match:
-#             new_str = re.sub(NON_ALPHA_PATTERN,"",chunk)
-#             name_as_list[i] = new_str
+    'PERCENTAGE_PATTERN' : {
+        'pattern' : re.compile(r"^[a-zA-Z]ercent[age]*"),
+        'replace_string' : 'Pct'
+    },
+
+    'DRIVING_PATTERN' : {
+        'pattern' : re.compile(r"^[a-zA-Z]riv[^s]*"),
+        'replace_string' : 'Drv'
+    },
+
+    'YARDAGE_PATTERN' : {
+        'pattern' : re.compile(r"^[a-zA-Z]ard[^s]*"),
+        'replace_string' : 'Ydg'
+    },
     
-#     name_as_list.remove("_") if "_" in name_as_list else None
-#     name_as_list.remove("®") if "®" in name_as_list else None
-    
-#     stat_id_dict[stat_id] = '_'.join(name_as_list)
+    'FAIRWAY_PATTERN' : {
+        'pattern' : re.compile(r"^[a-zA-Z]airway[^s]*"),
+        'replace_string' : 'Fwy'
+    },
 
+    'ROUGH_PATTERN' : { 
+        'pattern' : re.compile(r"^[rR]ough"),
+        'replace_string' : 'Rgh'
+    },
 
-# create_json_file("stat_id_dictionary.json", stat_id_dict)
+    'BIRDIE_PATTERN' : {
+        'pattern' : re.compile(r"[bB]irdie[^s]*"),
+        'replace_string' : 'Brd'
+    },
 
-with open("stat_id_dictionary.json") as stat_list:
+    'BOGEY_PATTERN' : {
+        'pattern' : re.compile(r"[bB]ogey[^es]*"),
+        'replace_string' : 'Bgy'
+    },
+
+    'PROXIMITY_PATTERN' : {
+        'pattern' : re.compile(r"[pP]roximity"),
+        'replace_string' : 'Prox'
+    },
+
+    'AVERAGE_PATTERN' : {
+        'pattern' : re.compile(r"[aA]verage"),
+        'replace_string' : 'Avg'
+    },
+
+    'EFFICIENCY_PATTERN' : {
+        'pattern' : re.compile(r"[eE]fficiency"),
+        'replace_string' : 'Eff'
+    },
+
+}
+
+with open("stat_list.json") as stat_list:
     all_stats = json.load(stat_list)
-print(len(all_stats))
-print(len(max(list(all_stats.values()),key=len)))
-print([f"len: {len(name)} : {name}" for name in list(all_stats.values()) if len(name) > 30])
+    categories = all_stats['pageProps']['statOverview']['categories']
+
+for category in categories:
+    for sub in category['subCategories']:
+        for stat in sub['stats']:
+            if stat_id_dict.get(stat.get('statId')):
+                continue
+            else:
+                stat_id_dict[stat.get('statId')] = stat.get('statTitle')
+
+for stat_id, stat_name in stat_id_dict.items():
+    name_as_list = stat_name.split()
+
+    for i,chunk in enumerate(name_as_list):
+          
+        chunk = chunk.replace("-","_")
+        name_as_list[i] = chunk
+
+        new_str = chunk
+
+        for pattern in PATTERN_DICT.keys():
+            if PATTERN_DICT[pattern]['pattern'].search(new_str):
+                new_str = re.sub(PATTERN_DICT[pattern]['pattern'], PATTERN_DICT[pattern]['replace_string'], new_str)
+        # if NON_ALPHA_PATTERN.search(new_str):
+        #     new_str = re.sub(NON_ALPHA_PATTERN,"",new_str)
+
+        # if APPROACH_PATTERN.search(new_str):
+        #     new_str = re.sub(APPROACH_PATTERN,"App",new_str)
+
+        # if PERCENTAGE_PATTERN.search(new_str):
+        #     new_str = re.sub(PERCENTAGE_PATTERN,"Pct",new_str)
+
+        # if DRIVING_PATTERN.search(new_str):
+        #     new_str = re.sub(DRIVING_PATTERN,"Drv",new_str)
+        
+        # if YARDAGE_PATTERN.search(new_str):
+        #     new_str = re.sub(YARDAGE_PATTERN,"Yrd",new_str)
+        
+        # if FAIRWAY_PATTERN.search(new_str):
+        #     new_str = re.sub(FAIRWAY_PATTERN,"Fwy",new_str)
+        
+        # if ROUGH_PATTERN.search(new_str):
+        #     new_str = re.sub(ROUGH_PATTERN,"Rgh",new_str)
+        
+        # if BIRDIE_PATTERN.search(new_str):
+        #     new_str = re.sub(BIRDIE_PATTERN,"Brd",new_str)
+
+        # if BOGEY_PATTERN.search(new_str):
+        #     new_str = re.sub(BOGEY_PATTERN,"Bgy",new_str)
+
+        # if PROXIMITY_PATTERN.search(new_str):
+        #     new_str = re.sub(PROXIMITY_PATTERN,"Prox",new_str)
+
+        # if AVERAGE_PATTERN.search(new_str):
+        #     new_str = re.sub(AVERAGE_PATTERN,"Avg",new_str)
+        
+        # if EFFICIENCY_PATTERN.search(new_str):
+        #     new_str = re.sub(EFFICIENCY_PATTERN,"Eff",new_str) 
+
+        
+        name_as_list[i] = new_str
+
+
+    for string in EXTRA_WORDS:
+        name_as_list.remove(string) if string in name_as_list else None
+    
+    stat_id_dict[stat_id] = '_'.join(name_as_list).lower()
+
+
+create_json_file("stat_id_dictionary.json", stat_id_dict)
+
+# with open("stat_id_dictionary.json") as stat_list:
+#     all_stats = json.load(stat_list)
+# print(len(all_stats))
+# print(len(max(list(all_stats.values()),key=len)))
+# print([f"len: {len(name)} : {name}" for name in list(all_stats.values()) if len(name) > 30])
 
 
 
