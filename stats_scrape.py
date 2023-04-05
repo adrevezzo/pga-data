94615cb15ed89620dd742089f4b3d27a15324813
Adam Revezzo <adam.revezzo@gmail.com>
2023-04-05 15:19:00 -0400

    create loop for stat table creation

--- stats_scrape.py
+++ stats_scrape.py
@@ -155,88 +155,54 @@ with open("stat_id_dictionary.json") as stat_list:
 # print(len(max(list(all_stats.values()),key=len)))
 # print([f"len: {len(name)} : {name}" for name in list(all_stats.values()) if len(name) > 30])
 
+with open("drv_dist_example.json") as stat_example:
+    ex = json.load(stat_example)
 
 stat_headers = ['id', 'player', 'avg', 'total_distance', 'total_drives']
 
 stat_types = ['SERIAL PRIMARY KEY', 'VARCHAR(255)', 'VARCHAR(255)', 'VARCHAR(255)', 'VARCHAR(255)']
 
 
+url = "https://orchestrator.pgatour.com/graphql"
+with open("stats_header.json") as data_completed:
+    headers = json.load(data_completed)
 
 
 with Database(db_type='dev') as (db, con, cur):
-    db.create_table("test2", stat_headers, stat_types)
+    select_query = """
+    SELECT id, pga_id, first_name || ' ' || last_name as player_name
+    from players
+    """
+    cur.execute(select_query)
+    player_id_results = cur.fetchall()
+
+    select_query = """
+    SELECT id, pga_tournament_id
+    from tournaments
+    """
+    cur.execute(select_query)
+    tournament_id_results = cur.fetchall()
 
 
-# url = "https://orchestrator.pgatour.com/graphql"
-
-# with open("schedule_header.json") as data_completed:
-#     headers = json.load(data_completed)
+player_id_dict = {result.get("pga_id"):int(result.get("id")) for result in player_id_results }
+tournament_id_dict = {result.get("pga_tournament_id"):int(result.get("id")) for result in tournament_id_results }
 
 
+with Database(db_type='dev') as (db, con, cur):
+    for stat in list(all_stats.keys())[:2]:
+        stat_headers = ['id', 'pga_stat_id', 'pga_stat_name', 'player_id', 'thru_tournament_id']
+        stat_types = ['SERIAL PRIMARY KEY', 'VARCHAR(255)', 'INT', 'VARCHAR(255)', 'INT']
+        payload = f"{{\"query\":\"query StatDetails($tourCode: TourCode!, $statId: String!, $year: Int, $eventQuery: StatDetailEventQuery) {{\\n  statDetails(\\n    tourCode: $tourCode\\n    statId: $statId\\n    year: $year\\n    eventQuery: $eventQuery\\n  ) {{\\n    tourCode\\n    year\\n    displaySeason\\n    statId\\n    statType\\n    tournamentPills {{\\n      tournamentId\\n      displayName\\n    }}\\n    yearPills {{\\n      year\\n      displaySeason\\n    }}\\n    statTitle\\n    statDescription\\n    tourAvg\\n    lastProcessed\\n    statHeaders\\n    statCategories {{\\n      category\\n      displayName\\n      subCategories {{\\n        displayName\\n        stats {{\\n          statId\\n          statTitle\\n        }}\\n      }}\\n    }}\\n    rows {{\\n      ... on StatDetailsPlayer {{\\n        __typename\\n        playerId\\n        playerName\\n        country\\n        countryFlag\\n        rank\\n        rankDiff\\n        rankChangeTendency\\n        stats {{\\n          statName\\n          statValue\\n          color\\n        }}\\n      }}\\n      ... on StatDetailTourAvg {{\\n        __typename\\n        displayName\\n        value\\n      }}\\n    }}\\n    sponsorLogo\\n  }}\\n}}\",\"operationName\":\"StatDetails\",\"variables\":{{\"tourCode\":\"R\",\"statId\":\"{stat}\",\"year\":2023,\"eventQuery\":{{\"queryType\":\"THROUGH_EVENT\",\"tournamentId\":\"R2023007\"}}}}}}"
+        time.sleep(3)
+        response = requests.request("POST", url, data=payload, headers=headers)
+        stats = response.json()
 
+        for stat_header in stats['data']['statDetails']['statHeaders']:
+            stat_headers.append(stat_header)
+            stat_types.append("VARCHAR(255)")
 
-# with Database(db_type='dev') as (con, cur):
-#     select_tournament_ids = """
-#     SELECT
-#     id,
-#     pga_tournament_id
-#     FROM tournaments
-#     /* WHERE id > 80 */
-#     """
+        # print(stat_headers,"\n",stat_types)
 
-#     cur.execute(select_tournament_ids)
-#     results = cur.fetchall()
+        table_name = all_stats.get(stat)
 
-#     for result in results:
-#         print(result)
-#         insert_course_data = """
-#         INSERT INTO courses (
-#             course_name,
-#             course_id_pga,
-#             city,
-#             state,
-#             country
-#         )
-        
-#         VALUES
-#         (%s, %s, %s, %s, %s)
-        
-#         """
-#         payload_for_course_id = f"{{\"query\":\"query Tournaments($ids: [ID!]) {{\\n  tournaments(ids: $ids) {{\\n    id\\n    tournamentName\\n    tournamentLogo\\n    tournamentLocation\\n    tournamentStatus\\n    roundStatusDisplay\\n    roundDisplay\\n    roundStatus\\n    roundStatusColor\\n    currentRound\\n    timezone\\n    pdfUrl\\n    seasonYear\\n    displayDate\\n    country\\n    state\\n    city\\n    scoredLevel\\n    events {{\\n      id\\n      eventName\\n      leaderboardId\\n    }}\\n    courses {{\\n      id\\n      courseName\\n      courseCode\\n      hostCourse\\n      scoringLevel\\n    }}\\n    weather {{\\n      logo\\n      logoDark\\n      logoAccessibility\\n      tempF\\n      tempC\\n      condition\\n      windDirection\\n      windSpeedMPH\\n      windSpeedKPH\\n      precipitation\\n      humidity\\n    }}\\n    ticketsURL\\n    tournamentSiteURL\\n    formatType\\n    features\\n  }}\\n}}\",\"operationName\":\"Tournaments\",\"variables\":{{\"ids\":\"{result.get('pga_tournament_id')}\"}}}}" 
-#         time.sleep(7)
-#         response = requests.request("POST", url, data=payload_for_course_id, headers=headers)
-        
-        
-#         all_data = response.json()
-
-#         try:
-#             tournament_data = all_data['data']['tournaments'][0]
-#         except TypeError:
-#             tournaments_without_course.append(result.get('pga_tournament_id'))
-#         else:
-            
-#             for course in tournament_data['courses']:
-#                 if course['id'] in courses_added:
-#                     continue
-#                 else:
-#                     course_name = course['courseName']
-#                     course_id_pga = course['id']
-#                     city = tournament_data['city']
-#                     state = tournament_data['state']
-#                     country = tournament_data['country']
-
-#                     data_completed = (
-#                         course_name, 
-#                         course_id_pga,
-#                         city,
-#                         state,
-#                         country
-#                         )
-
-#                     cur.execute(insert_course_data, data_completed)
-#                     con.commit()
-#                     courses_added.append(course_id_pga)
-
-
-
-
+        db.create_table(table_name, stat_headers, stat_types)
